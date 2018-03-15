421295471ffab3ba8675aeb4e2d35ad334fcb249
JTorrus <jtorrusgalindo@gmail.com>
2018-03-15 15:22:07 +0100

    Initial Commit

---
+++ SallePlaces/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SallePlaces
+//
+//  Created by Alumne on 15/3/18.
+//  Copyright © 2018 Alumne. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
